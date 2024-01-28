a34b2bed8bae82ac317892da0c5c7e96a0de39a9
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-28 16:34:02 +0800

    移动方块

--- MoveBox/MainWindow.xaml.cs
+++ MoveBox/MainWindow.xaml.cs
@@ -20,5 +20,55 @@ namespace MoveBox
         {
             InitializeComponent();
         }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            // 首先判断按键的方位 比如上下左右
+            // 向上移动 移动格子每次减3 向下移动 移动格子每次加3
+            // 向左一定 每次减1 向右移动 每次加1
+
+            // 获取白色的border元素
+            UIElementCollection children = gridContent.Children;
+            Border curBorder = null;
+            for (int i = 0; i < children.Count; i++)
+            {
+                if (children[i] is Border)
+                {
+                    if (((children[i] as Border).Background as SolidColorBrush).Color.Equals(Colors.White))
+                    {
+                        curBorder = children[i] as Border;
+                    }
+                    (children[i] as Border).Background = new SolidColorBrush(Colors.Transparent);
+                }
+            }
+
+            string name = curBorder.Name;
+            int index = Convert.ToInt32(name.Replace("b", ""));
+
+            if (e.Key.Equals(Key.Up))
+            {
+                index = index - 3 >= 1 ? index - 3 : index;
+            }
+            else if (e.Key.Equals(Key.Down))
+            {
+                index = index + 3 <= 9 ? index + 3 : index;
+            }
+            else if (e.Key.Equals(Key.Left))
+            {
+                index = index - 1 >= 1 ? index - 1 : index;
+            }
+            else
+            {
+                index = index - 1 <= 9 ? index + 1 : index;
+            }
+
+            // 讲border的背景色改成白色
+            object control = gridContent.FindName("b" + index);
+            if (control != null)
+            {
+                (control as Border).Background = new SolidColorBrush(Colors.White);
+            }
+
+        }
     }
 }
