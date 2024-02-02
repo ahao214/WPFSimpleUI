34656216c35e28b4a06aeb5a07c0daec98227e75
liupenghao@fulan.com.cn <haochen214@163.com>
2024-02-02 21:44:14 +0800

    SliderControl

---
+++ SliderControl/RiotSlider.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SliderControl
+{
+
+    public class RiotSlider : Slider
+    {
+        static RiotSlider()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RiotSlider), new FrameworkPropertyMetadata(typeof(RiotSlider)));
+        }
+    }
+}
